d8ac50aa1dbfd05dffd53a04082e35680ee3c7ba
vongnguyen0933 <vongnguyen0933@gmail.com>
2023-10-05 17:00:06 +0700

    add_css_baseLine

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,12 +16,20 @@ module.exports = {
     'react-refresh'
   ],
   rules: {
+    //React
     'react-refresh/only-export-components': 'warn',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 0,
     'react/display-name': 0,
-
+    //Mui
+    'no-restricted-imports': [
+      'error',
+      {
+        'patterns': ['@mui/*/*/*']
+      }
+    ],
+    //Common
     'no-console': 1,
     'no-lonely-if': 1,
     'no-unused-vars': 1,
@@ -39,6 +47,7 @@ module.exports = {
     'keyword-spacing': 1,
     'comma-dangle': 1,
     'comma-spacing': 1,
-    'arrow-spacing': 1
+    'arrow-spacing': 1,
+
   }
 }
