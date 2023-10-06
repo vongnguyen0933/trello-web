0874f4474ab9cb044215a46bf2d50faa6a860a18
vongnguyen0933 <vongnguyen0933@gmail.com>
2023-10-06 19:14:19 +0700

    dark_ligt_mode_p2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,63 @@
 /* eslint-disable react/jsx-no-target-blank */
+import { useColorScheme } from '@mui/material/styles'
 import { Typography } from '@mui/material'
 import Button from '@mui/material/Button'
-import {
-  useColorScheme
-} from '@mui/material/styles';
 
 
+import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+
+
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+
+
+  const handleChange = (event) => {
+    const setModeSelect = event.target.value
+    setMode(setModeSelect)
+  }
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl >
+        <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+        <Select
+          labelId="label-select-dark-light-mode"
+          id="demo-simple-select"
+          value={mode}
+          label="Mode"
+          onChange={handleChange}
+        >
+          <MenuItem value="light">
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <LightModeIcon /> Light
+            </div>
+          </MenuItem>
+          <MenuItem value="dark">
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <DarkModeIcon /> Dark
+            </div>
+          </MenuItem>
+          <MenuItem value="system">
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <SettingsBrightnessIcon /> System
+            </div>
+          </MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  )
+}
 function ModeToggle() {
-  const { mode, setMode } = useColorScheme();
+  const { mode, setMode } = useColorScheme()
   return (
     <Button
       onClick={() => {
@@ -24,6 +74,8 @@ function App() {
 
   return (
     <>
+      <ModeSelect />
+      <hr />
       <ModeToggle />
       <hr />
       <div>vongnguyendev</div>
