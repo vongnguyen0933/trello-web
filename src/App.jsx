4d8b018011489880dfe04e05a2010e752dc65348
vongnguyen0933 <vongnguyen0933@gmail.com>
2023-10-06 18:07:58 +0700

    d_l_mode_p1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 /* eslint-disable react/jsx-no-target-blank */
+import { Typography } from '@mui/material'
 import Button from '@mui/material/Button'
+import {
+  useColorScheme
+} from '@mui/material/styles';
+
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
 
 function App() {
 
   return (
     <>
+      <ModeToggle />
+      <hr />
       <div>vongnguyendev</div>
+      <Typography variant='body2' color='text.secondary' >Text</Typography>
       <Button variant="contained">Hello world</Button>
     </>
   )
